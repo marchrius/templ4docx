2614fee2720f641f9c1d187d201b52dd0461d1a6
Matteo Gaggiano <maxxxx92@gmail.com>
2018-11-07 15:53:23 +0100

    Added another constructor to help creation of DocumentVariable

--- src/main/java/pl/jsolve/templ4docx/variable/DocumentVariable.java
+++ src/main/java/pl/jsolve/templ4docx/variable/DocumentVariable.java
@@ -1,23 +1,29 @@
 package pl.jsolve.templ4docx.variable;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import pl.jsolve.templ4docx.core.Docx;
 
 public class DocumentVariable implements Variable {
 
-	private final String key;
-	private final XWPFDocument document;
-	
-	public DocumentVariable(String key, XWPFDocument document) {
-		this.key = key;
-		this.document = document;
-	}
+  private final String key;
+  private final XWPFDocument document;
 
-	public String getKey() {
-		return key;
-	}
+  public DocumentVariable(String key, Docx document) {
+    this.key = key;
+    this.document = document.getXWPFDocument();
+  }
 
-	public XWPFDocument getDocument() {
-		return document;
-	}
-	
+  public DocumentVariable(String key, XWPFDocument document) {
+    this.key = key;
+    this.document = document;
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public XWPFDocument getDocument() {
+    return document;
+  }
+
 }
