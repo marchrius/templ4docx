7e58fdcb95f42023b2d0f97d463344664018b95e
Matteo Gaggiano <maxxxx92@gmail.com>
2018-11-07 15:51:16 +0100

    Added documentParent reference into DocumentInsert

--- src/main/java/pl/jsolve/templ4docx/insert/DocumentInsert.java
+++ src/main/java/pl/jsolve/templ4docx/insert/DocumentInsert.java
@@ -1,19 +1,20 @@
 package pl.jsolve.templ4docx.insert;
 
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
 import pl.jsolve.templ4docx.util.Key;
 
-public class DocumentInsert extends Insert{
+public class DocumentInsert extends ParagraphInsert {
 
-	 private XWPFParagraph paragraph;
-
-    public DocumentInsert(Key key, XWPFParagraph paragraph) {
-        super(key);
-        this.paragraph = paragraph;
+    public DocumentInsert(Key key, XWPFParagraph paragraph, XWPFTableCell cellParent, XWPFDocument documentParent, boolean inAList) {
+        super(key, paragraph, cellParent, documentParent);
+        this.setInAList(inAList);
     }
 
-    public XWPFParagraph getParagraph() {
-        return paragraph;
+    public DocumentInsert(Key key, XWPFParagraph paragraph, XWPFTableCell cellParent, XWPFDocument documentParent) {
+        super(key, paragraph, cellParent, documentParent);
+        this.setInAList(false);
     }
 }
--- src/main/java/pl/jsolve/templ4docx/strategy/BulletListInsertStrategy.java
+++ src/main/java/pl/jsolve/templ4docx/strategy/BulletListInsertStrategy.java
@@ -1,8 +1,8 @@
 package pl.jsolve.templ4docx.strategy;
 
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.*;
 import org.apache.xmlbeans.XmlCursor;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTAbstractNum;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTPPr;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTRPr;
 
@@ -23,89 +23,94 @@ import pl.jsolve.templ4docx.variable.ObjectVariable;
 import pl.jsolve.templ4docx.variable.TextVariable;
 import pl.jsolve.templ4docx.variable.Variable;
 
+import java.math.BigInteger;
+
 public class BulletListInsertStrategy implements InsertStrategy {
 
-    private InsertStrategyChooser insertStrategyChooser;
-    private ParagraphCleaner paragraphCleaner;
+  private InsertStrategyChooser insertStrategyChooser;
+  private ParagraphCleaner paragraphCleaner;
 
-    public BulletListInsertStrategy(InsertStrategyChooser insertStrategyChooser, ParagraphCleaner paragraphCleaner) {
-        this.insertStrategyChooser = insertStrategyChooser;
-        this.paragraphCleaner = paragraphCleaner;
+  public BulletListInsertStrategy(InsertStrategyChooser insertStrategyChooser,
+      ParagraphCleaner paragraphCleaner) {
+    this.insertStrategyChooser = insertStrategyChooser;
+    this.paragraphCleaner = paragraphCleaner;
+  }
+
+  @Override
+  public void insert(Insert insert, Variable variable) {
+    if (!(insert instanceof BulletListInsert)) {
+      return;
+    }
+    if (!(variable instanceof BulletListVariable)) {
+      return;
     }
 
-    @Override
-    public void insert(Insert insert, Variable variable) {
-        if (!(insert instanceof BulletListInsert)) {
-            return;
-        }
-        if (!(variable instanceof BulletListVariable)) {
-            return;
-        }
+    BulletListInsert bulletListInsert = (BulletListInsert) insert;
+    BulletListVariable bulletListVariable = (BulletListVariable) variable;
+    XWPFParagraph templateParagraph = bulletListInsert.getParagraph();
 
-        BulletListInsert bulletListInsert = (BulletListInsert) insert;
-        BulletListVariable bulletListVariable = (BulletListVariable) variable;
-        XWPFParagraph templateParagraph = bulletListInsert.getParagraph();
+    XWPFParagraph copiedParagraph = null;
+    for (int i = 0; i < bulletListVariable.getVariables().size(); i++) {
+      XmlCursor templateCursor = templateParagraph.getCTP().newCursor();
+      copiedParagraph = templateParagraph.getBody().insertNewParagraph(templateCursor);
+      cloneParagraph(copiedParagraph, templateParagraph);
 
-        XWPFParagraph copiedParagraph = null;
-        for (int i = 0; i < bulletListVariable.getVariables().size(); i++) {
-            XmlCursor templateCursor = templateParagraph.getCTP().newCursor();
-            copiedParagraph = templateParagraph.getBody().insertNewParagraph(templateCursor);
-            cloneParagraph(copiedParagraph, templateParagraph);
-
-            Variable variableToInsert = bulletListVariable.getVariables().get(i);
-            Insert copiedInsert = prepareInsert(bulletListInsert, copiedParagraph, bulletListInsert.getKey(),
-                    variableToInsert);
-            insertStrategyChooser.replace(copiedInsert, variableToInsert);
-        }
-
-        bulletListInsert.deleteMe();
+      Variable variableToInsert = bulletListVariable.getVariables().get(i);
+      Insert copiedInsert = prepareInsert(bulletListInsert, copiedParagraph,
+          bulletListInsert.getKey(),
+          variableToInsert);
+      insertStrategyChooser.replace(copiedInsert, variableToInsert);
     }
 
-    private void cloneParagraph(XWPFParagraph clone, XWPFParagraph source) {
-        CTPPr pPr = clone.getCTP().addNewPPr();
-        pPr.set(source.getCTP().getPPr());
-        for (XWPFRun r : source.getRuns()) {
-            XWPFRun newRun = clone.createRun();
-            cloneRun(newRun, r);
-        }
+    bulletListInsert.deleteMe();
+  }
+
+  private void cloneParagraph(XWPFParagraph clone, XWPFParagraph source) {
+    CTPPr pPr = clone.getCTP().addNewPPr();
+    pPr.set(source.getCTP().getPPr());
+    for (XWPFRun r : source.getRuns()) {
+      XWPFRun newRun = clone.createRun();
+      cloneRun(newRun, r);
+    }
+  }
+
+  private void cloneRun(XWPFRun clone, XWPFRun source) {
+    CTRPr rPr = clone.getCTR().addNewRPr();
+    rPr.set(source.getCTR().getRPr());
+    clone.setText(source.getText(0));
+  }
+
+  private Insert prepareInsert(BulletListInsert originalInsert, XWPFParagraph paragraph, Key key,
+      Variable variableToInsert) {
+    if (variableToInsert instanceof TextVariable) {
+      return new TextInsert(new Key(key.getKey(), VariableType.TEXT), paragraph);
+    }
+    if (variableToInsert instanceof ImageVariable) {
+      return new ImageInsert(new Key(key.getKey(), VariableType.IMAGE), paragraph);
+    }
+    if (variableToInsert instanceof BulletListVariable) {
+      return new BulletListInsert(new Key(key.getKey(), VariableType.BULLET_LIST), paragraph,
+          originalInsert.getCellParent(), originalInsert.getDocumentParent());
+    }
+    if (variableToInsert instanceof ObjectVariable) {
+      return new ObjectInsert(new Key(key.getKey(), VariableType.OBJECT), paragraph);
+    }
+    if (variableToInsert instanceof DocumentVariable) {
+      return new DocumentInsert(new Key(key.getKey(), VariableType.DOCUMENT),
+          paragraph, originalInsert.getCellParent(), originalInsert.getDocumentParent());
     }
 
-    private void cloneRun(XWPFRun clone, XWPFRun source) {
-        CTRPr rPr = clone.getCTR().addNewRPr();
-        rPr.set(source.getCTR().getRPr());
-        clone.setText(source.getText(0));
-    }
+    return null;
+  }
 
-    private Insert prepareInsert(BulletListInsert originalInsert, XWPFParagraph paragraph, Key key,
-            Variable variableToInsert) {
-        if (variableToInsert instanceof TextVariable) {
-            return new TextInsert(new Key(key.getKey(), VariableType.TEXT), paragraph);
-        }
-        if (variableToInsert instanceof ImageVariable) {
-            return new ImageInsert(new Key(key.getKey(), VariableType.IMAGE), paragraph);
-        }
-        if (variableToInsert instanceof BulletListVariable) {
-            return new BulletListInsert(new Key(key.getKey(), VariableType.BULLET_LIST), paragraph,
-                    originalInsert.getCellParent(), originalInsert.getDocumentParent());
-        }
-        if (variableToInsert instanceof ObjectVariable) {
-            return new ObjectInsert(new Key(key.getKey(), VariableType.OBJECT), paragraph);
-        }
-        if(variableToInsert instanceof DocumentVariable) {
-            return new DocumentInsert(new Key(key.getKey(), VariableType.DOCUMENT), paragraph);
-        }
-
-        return null;
+  public void cleanParagraphs() {
+    for (ParagraphInsert paragraph : paragraphCleaner.getParagraphs()) {
+      try {
+        paragraph.deleteMe();
+      } catch (Exception ex) {
+        // do nothing, row doesn't exist
+      }
     }
-
-    public void cleanParagraphs() {
-        for (ParagraphInsert paragraph : paragraphCleaner.getParagraphs()) {
-            try {
-                paragraph.deleteMe();
-            } catch (Exception ex) {
-                // do nothing, row doesn't exist
-            }
-        }
-    }
+  }
 
 }
