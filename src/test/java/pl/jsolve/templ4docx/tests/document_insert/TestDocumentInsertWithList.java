d460cca754ea80c1dea148387ea544fcc33ca0fe
Matteo Gaggiano <maxxxx92@gmail.com>
2018-11-09 09:59:04 +0100

    Fixed test check

--- src/test/java/pl/jsolve/templ4docx/tests/document_insert/TestDocumentInsertWithList.java
+++ src/test/java/pl/jsolve/templ4docx/tests/document_insert/TestDocumentInsertWithList.java
@@ -1,5 +1,8 @@
 package pl.jsolve.templ4docx.tests.document_insert;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 import org.junit.Test;
 import pl.jsolve.templ4docx.core.Docx;
 import pl.jsolve.templ4docx.core.VariablePattern;
@@ -43,10 +46,14 @@ public class TestDocumentInsertWithList extends AbstractDocumentInsertTest {
 
     Variables var = new Variables();
 
+    Date date = new Date(704750400000L);
+
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy 'at' HH:mm");
+
     var.addTextVariable("#{variableWithVeryVeryLongName01}", "Ciao");
     var.addTextVariable("#{variableWithVeryVeryVeryVeryVeryVeryLongName02}", "Mondo");
     var.addTextVariable("#{variableWithVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongName03}",
-        (new Date()).toString());
+        sdf.format(date));
 
     var.addDocumentVariable("#{document_a:document}", subADocx);
     var.addDocumentVariable("#{document_b:document}", subBDocx);
@@ -72,25 +79,57 @@ public class TestDocumentInsertWithList extends AbstractDocumentInsertTest {
 
     docx.save(processedPath);
 
-//    String text = docx.readTextContent();
-//
-//    System.out.println(text);
-//
-//        assertEquals("This is test simple template with three variables: #{var01}, #{var02}, #{var03}.\n" +
-//                "\n" +
-//                "This is test simple template with three variables with long names: ${variableWithVeryVeryLongName01}, ${variableWithVeryVeryVeryVeryVeryVeryLongName02}, ${variableWithVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongName03}.\n" +
-//                "\n" +
-//                "\n" +
-//                "This is test simple template with three variables with long names: ${variableWithVeryVeryLongName01}, ${variableWithVeryVeryVeryVeryVeryVeryLongName02}, ${variableWithVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongName03}.\n" +
-//                "\n" +
-//                "\n" +
-//                "This is test simple template with three variables with long names: ${variableWithVeryVeryLongName01}, ${variableWithVeryVeryVeryVeryVeryVeryLongName02}, ${variableWithVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLongName03}.\n" +
-//                "\n" +
-//                "\n" +
-//                "This document will cost you $ 1234.56\n" +
-//                "\n" +
-//                "IBAN / Account #:  1234.56",
-//                text.trim());
+    String text = docx.readTextContent();
+
+    System.out.println(text);
+
+        assertEquals("Document insert with list\n"
+                + "Insert a document into as a list element into pre-existent list\n"
+                + "This is a simple text followed by a multi-level list\n"
+                + "\n"
+                + "Level 1\n"
+                + "Level 1.1\n"
+                + "Level 2\n"
+                + "Level 2.1\n"
+                + "Level 2.2\n"
+                + "Level 2.2.1\n"
+                + "Level 2.2.1.1\n"
+                + "Level 3\n"
+                + "Level 3.1\n"
+                + "Level 3.1.1\n"
+                + "Level 3.1.2\n"
+                + "Level 4\n"
+                + "Level 4.1\n"
+                + "Level 5\n"
+                + "Level 5.1\n"
+                + "Level 5.1.1\n"
+                + "\n"
+                + "nullSimple text. Bold text. Italic text. Strike text. Red text. Glowing text. Text withnullcarriage return. This text will be inserted into a numerating list with an image\n"
+                + "The sun shines as it has never shone. This sentence is wrong, can you tell me why?\n"
+                + "Level 5.1.3\n"
+                + " nullSimple text. Bold text. Italic text. Strike text. Red text. Glowing text. Text withnullcarriage return. This text will be inserted into a numerating list with an image\n"
+                + "The sun shines as it has never shone. This sentence is wrong, can you tell me why?\n"
+                + "Testo 1 \n"
+                + "Testo 2 \n"
+                + "Testo 3 \n"
+                + "Testo 4 \n"
+                + "Testo 5 \n"
+                + "Level 6\n"
+                + "nullSimple text. Bold text. Italic text. Strike text. Red text. Glowing text. Text withnullcarriage return. This text will be inserted into a numerating list with an image\n"
+                + "The sun shines as it has never shone. This sentence is wrong, can you tell me why?\n"
+                + "This is test simple template with three variables with long names but with short values: Ciao, Mondo, " + sdf.format(date) + ".\n"
+                + "\n"
+                + "Here we have the document-a\n"
+                + "nullSimple text. Bold text. Italic text. Strike text. Red text. Glowing text. Text withnullcarriage return. This text will be inserted into a numerating list with an image\n"
+                + "The sun shines as it has never shone. This sentence is wrong, can you tell me why?\n"
+                + "\n"
+                + "Here we have the document b\n"
+                + "This is test simple template with three variables with long names but with short values: Ciao, Mondo, " + sdf.format(date) + ".\n"
+                + "\n"
+                + "Level 1\n"
+                + "Level 2\n"
+                + "Level 3",
+                text.trim());
   }
 
 }
