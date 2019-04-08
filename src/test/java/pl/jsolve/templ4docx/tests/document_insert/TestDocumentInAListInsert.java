855bdea272c84c6126486a6647375cf28200c04f
Matteo Gaggiano <maxxxx92@gmail.com>
2019-04-08 12:52:16 +0200

    Updated test to better corresponds test

--- src/test/java/pl/jsolve/templ4docx/tests/document_insert/TestDocumentInAListInsert.java
+++ src/test/java/pl/jsolve/templ4docx/tests/document_insert/TestDocumentInAListInsert.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.poi.util.IOUtils;
 import org.junit.Test;
 import pl.jsolve.templ4docx.core.Docx;
 import pl.jsolve.templ4docx.core.VariablePattern;
@@ -28,11 +29,18 @@ public class TestDocumentInAListInsert extends AbstractDocumentInsertTest {
     InputStream subIs = getClass().getClassLoader()
         .getResourceAsStream(subDocumentFileName + ".docx");
 
+    String subDocumentAsListFileName = "document-insert/document-template-document-with-paragraph-as-list";
+    InputStream subAsListIs = getClass().getClassLoader()
+        .getResourceAsStream(subDocumentAsListFileName + ".docx");
+
     Docx docx = new Docx(is);
-    is.close();
+    IOUtils.closeQuietly(is);
 
     Docx subDocx = new Docx(subIs);
-    subIs.close();
+    IOUtils.closeQuietly(subIs);
+
+    Docx subAsListDocx = new Docx(subAsListIs);
+    IOUtils.closeQuietly(subAsListIs);
 
     docx.setVariablePattern(new VariablePattern("#{", "}"));
 
@@ -41,14 +49,26 @@ public class TestDocumentInAListInsert extends AbstractDocumentInsertTest {
     var.addDocumentVariable("#{document.1.1}", subDocx, true);
     var.addDocumentVariable("#{document.1.2}", subDocx, false);
 
+    var.addDocumentVariable("#{document.2.1}", subAsListDocx, true);
+    var.addDocumentVariable("#{document.2.2}", subAsListDocx, false);
+
     List<Variable> listAsUniqueParam = new ArrayList<Variable>();
     List<Variable> listAsNotUniqueParam = new ArrayList<Variable>();
+
+    List<Variable> listAsListAsUniqueParam = new ArrayList<Variable>();
+    List<Variable> listAsListAsNotUniqueParam = new ArrayList<Variable>();
 
     listAsUniqueParam.add(new DocumentVariable("#{document.uniquepraram}", subDocx, true));
     listAsNotUniqueParam.add(new DocumentVariable("#{document.nonuniquepraram}", subDocx, false));
 
+    listAsListAsUniqueParam.add(new DocumentVariable("#{document.uniquepraram.aslist}", subAsListDocx, true));
+    listAsListAsNotUniqueParam.add(new DocumentVariable("#{document.nonuniquepraram.aslist}", subAsListDocx, false));
+
     var.addBulletListVariable("#{document.uniquepraram}", listAsUniqueParam);
     var.addBulletListVariable("#{document.nonuniquepraram}", listAsNotUniqueParam);
+
+    var.addBulletListVariable("#{document.uniquepraram.aslist}", listAsListAsUniqueParam);
+    var.addBulletListVariable("#{document.nonuniquepraram.aslist}", listAsListAsNotUniqueParam);
 
     List<String> placeholders = docx.findVariables();
 
@@ -65,11 +85,21 @@ public class TestDocumentInAListInsert extends AbstractDocumentInsertTest {
     System.out.println(text);
 
     assertEquals(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate ultricies felis, a euismod leo suscipit eget\n"
+        "Dump document insertion for document.1.1/2\n"
+            + "\n"
+            + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate ultricies felis, a euismod leo suscipit eget\n"
             + "Quisque maximus dictum interdum. Nulla facilisi. Suspendisse gravida est sed auctor ullamcorper.\n"
             + "\n"
             + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate ultricies felis, a euismod leo suscipit eget\n"
             + "Quisque maximus dictum interdum. Nulla facilisi. Suspendisse gravida est sed auctor ullamcorper.\n"
+            + "Dump document insertion for document.2.1/2\n"
+            + "\n"
+            + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate ultricies felis, a euismod leo suscipit eget\n"
+            + "Quisque maximus dictum interdum. Nulla facilisi. Suspendisse gravida est sed auctor ullamcorper.\n"
+            + "\n"
+            + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate ultricies felis, a euismod leo suscipit eget\n"
+            + "Quisque maximus dictum interdum. Nulla facilisi. Suspendisse gravida est sed auctor ullamcorper.\n"
+            + "Forced unique paragraph for document.uniqueparam\n"
             + "\n"
             + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate ultricies felis, a euismod leo suscipit eget\n"
             + "Quisque maximus dictum interdum. Nulla facilisi. Suspendisse gravida est sed auctor ullamcorper.\n"
@@ -78,6 +108,25 @@ public class TestDocumentInAListInsert extends AbstractDocumentInsertTest {
             + "Item 3\n"
             + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate ultricies felis, a euismod leo suscipit eget\n"
             + "Quisque maximus dictum interdum. Nulla facilisi. Suspendisse gravida est sed auctor ullamcorper.\n"
+            + "Forced unique paragraph for document.uniqueparam.aslist\n"
+            + "\n"
+            + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate ultricies felis, a euismod leo suscipit eget\n"
+            + "Quisque maximus dictum interdum. Nulla facilisi. Suspendisse gravida est sed auctor ullamcorper.\n"
+            + "Item 1\n"
+            + "Item 2\n"
+            + "Item 3\n"
+            + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate ultricies felis, a euismod leo suscipit eget\n"
+            + "Quisque maximus dictum interdum. Nulla facilisi. Suspendisse gravida est sed auctor ullamcorper.\n"
+            + "Non-unique paragraph for document.nonuniqueparam\n"
+            + "\n"
+            + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate ultricies felis, a euismod leo suscipit eget\n"
+            + "Quisque maximus dictum interdum. Nulla facilisi. Suspendisse gravida est sed auctor ullamcorper.\n"
+            + "Item 1\n"
+            + "Item 2\n"
+            + "Item 3\n"
+            + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate ultricies felis, a euismod leo suscipit eget\n"
+            + "Quisque maximus dictum interdum. Nulla facilisi. Suspendisse gravida est sed auctor ullamcorper.\n"
+            + "Non-unique paragraph for document.nonuniqueparam.aslist\n"
             + "\n"
             + "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate ultricies felis, a euismod leo suscipit eget\n"
             + "Quisque maximus dictum interdum. Nulla facilisi. Suspendisse gravida est sed auctor ullamcorper.\n"
